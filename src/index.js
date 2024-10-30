dd5e30da0bf50ea70a69dc84be9f55fd0226031c
asjasj3964 <asjasj3964@naver.com>
2024-10-30 23:46:02 +0900

    mission: 4주차 미션 (주석 추가)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 // const express = require('express') // CommonJS
 import express from 'express' // ES Module
 const app = express()
-const port = 3000
+const port = 3000 // 서버 실행 포트를 3000번으로 지정
 
+// GET 요청 처리 라우트 설정
 app.get('/', (req, res) => {
-  res.send('Hello World! UMC Wenty')
+  res.send('Hello World! UMC Wenty') // 메시지를 클라이언트에 응답
 })
 
+// 서버 실행
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
